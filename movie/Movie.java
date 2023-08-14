63fb7c425f5db20745a817840cc01719b1245e82
heegyun <yhg1124@daum.net>
2023-08-14 11:51:35 +0900

    Add files via upload

--- movie/Movie.java
+++ movie/Movie.java
@@ -1,8 +1,7 @@
-package my.movie;
+package movie;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
